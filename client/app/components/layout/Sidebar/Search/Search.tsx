828cf3453fbaed8faffb595e2abc10b6d74881b3
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2022-05-31 19:32:27 +0100

    connected react query devtools & on search component if input empty clear search list

--- client/app/components/layout/Sidebar/Search/Search.tsx
+++ client/app/components/layout/Sidebar/Search/Search.tsx
@@ -8,10 +8,11 @@ import SearchField from '@/ui/search-field/SearchField'
 
 const Search: FC = () => {
 	const { isSuccess, data, handleSearch, searchTerm } = useSearch()
+
 	return (
 		<div className={styles.wrapper}>
 			<SearchField searchTerm={searchTerm} handleSearch={handleSearch} />
-			{isSuccess && <SearchList movies={data || []} />}
+			{isSuccess && searchTerm !== '' && <SearchList movies={data || []} />}
 		</div>
 	)
 }
--- client/app/providers/MainProvider.tsx
+++ client/app/providers/MainProvider.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import Layout from '@/components/layout/Layout'
 
@@ -18,6 +19,7 @@ type IProviderProps = {
 const MainProvider: FC<IProviderProps> = ({ children }) => {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<Layout>{children}</Layout>
 		</QueryClientProvider>
 	)
