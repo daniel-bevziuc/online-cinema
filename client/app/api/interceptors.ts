7bf3866fdee8d2359b724fcfd1a2a3e5de2d6402
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2022-06-04 12:58:12 +0100

    added interceptors & admin/profile page plain template

--- client/app/api/interceptors.ts
+++ client/app/api/interceptors.ts
@@ -1,9 +1,52 @@
+import { AuthService } from '@/services/auth/auth.service';
 import { API_URL } from '@/config/api.config'
 import axios from 'axios'
+import Cookies from 'js-cookie'
+import { errorCatch, getContentType } from './api.helper'
+import { removeTokensStorage } from '@/services/auth/auth.helper';
 
 export const axiosClassic = axios.create({
   baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  headers: getContentType()
 })
+
+export const instance = axios.create({
+  baseURL: API_URL,
+  headers: getContentType()
+})
+
+instance.interceptors.request.use((config) => {
+  const accessToken = Cookies.get('accessToken')
+  if (config.headers && accessToken)
+    config.headers.Authorization = `Bearer ${accessToken}`
+
+  return config
+})
+
+instance.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config
+
+    if (
+      (error.response.status === 401 ||
+        errorCatch(error) === 'jwt expired' ||
+        errorCatch(error) === 'jwt must be provided') &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true
+      try {
+        await AuthService.getNewTokens()
+
+        return instance.request(originalRequest)
+      } catch (e) {
+        if (errorCatch(e) === 'jwt expired') removeTokensStorage()
+      }
+    }
+
+    throw error
+  }
+)
+
+export default instance
---
+++ client/pages/manage/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const AdminPage: NextPage = () => {
+	return <div>index</div>
+}
+
+export default AdminPage
